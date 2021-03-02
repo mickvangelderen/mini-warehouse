6b2a4028015fa669ba06bf424f55db59d2253f18
Mick van Gelderen <mickvangelderen@gmail.com>
2021-03-03 00:45:34 +0100

    More math

--- src/math.ts
+++ src/math.ts
@@ -30,6 +30,31 @@ export class Vector2 {
         this.x *= n;
         this.y *= n;
     }
+
+    round(): Vector2 {
+        return new Vector2(
+            Math.round(this.x),
+            Math.round(this.y),
+        );
+    }
+
+    floor(): Vector2 {
+        return new Vector2(
+            Math.floor(this.x),
+            Math.floor(this.y),
+        );
+    }
+
+    to_point(): Point2 {
+        return new Point2(
+            this.x,
+            this.y
+        );
+    }
+
+    static from_scalar(n: number): Vector2 {
+        return new Vector2(n, n);
+    }
 }
 
 export class Point2 {
@@ -51,5 +76,9 @@ export class Point2 {
     add_assign(v: Vector2): void {
         this.x += v.x;
         this.y += v.y;
+    }
+
+    to_vector(): Vector2 {
+        return new Vector2(this.x, this.y);
     }
 }
